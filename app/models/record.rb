28cacfb324f0d7ce7da9070f3d0184af36ae915a
Julia Van Cleve <julia@jmv.me>
2015-08-12 17:07:03 -0700

    Completed top_referrers

--- app/models/record.rb
+++ app/models/record.rb
@@ -20,19 +20,41 @@ class Record < ActiveRecord::Base
   end
   
   def self.top_referrers
-    end_day = DateTime.current
+    # Get array of dates to group hash by
+    end_day = DateTime.current.utc.to_date
     start_day = end_day - 4
     date_array = get_dates(start_day, end_day)
+    
+    # initialize hash
     obj = Hash.new{|hash, key| hash[key] = Array.new}
+    
+    # loop over each date in array and construct a query to get top 10 urls
     date_array.each do |the_date|
-      records = Record.select("url, count('url') AS total, date(created_at)").where('date(created_at) = ?', the_date).group("date(created_at)").group(:url).order('date(created_at) DESC').order("count('url') DESC").limit(10)
+      top_ten = Record.select("url, count(url) AS total").where('date_created_at = ?', the_date).group(:url).order("total DESC").limit(10)
       
-      records.each do |record|
+      # loop over each top_10 url and query for their referrer urls and counts
+      top_ten.each do |t|
         url_visits = {}
-        url_visits.store(:url, record.url)
-        url_visits.store(:visits, record.total)
+        url_visits.store(:url, t.url)
+        url_visits.store(:visits, t.total)
+        
+        refs = []
+        # Query for top 5 referrers on a particular day and for a particular url
+        referrers = Record.select("referrer, count(referrer) AS ref_total").where('date_created_at = ? and url = ?', the_date, t.url).group(:referrer).order(:url).order('ref_total DESC').limit(5)
+        referrers.each do |r|
+          # create referrer url-visits hash
+          ref_visits = {}
+          ref_visits.store(:url, r.referrer)
+          ref_visits.store(:visits, r.ref_total)
+          # add to array of referrers
+          refs.push(ref_visits)
+        end
+        # push array of referrers onto url_visits
+        url_visits.store(:referrers, refs)
+        
         obj[the_date].push(url_visits)
       end
+      
      end
     return obj
   end
@@ -40,7 +62,7 @@ class Record < ActiveRecord::Base
   private
   
     def self.get_dates(start_date, end_date)
-      return (start_date.to_date..end_date.to_date).map{ |date| date.strftime("%Y-%m-%d") }
+      return (start_date.to_date..end_date).map{ |date| date.strftime("%Y-%m-%d") }
     end
   
     def create_hash
